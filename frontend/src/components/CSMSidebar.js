255d84c9d7bd340ee8b525076851f6eaed0e3fb5
thamidu-peiris <thamidu2c@gmail.com>
2025-05-05 13:17:41 +0530

    CSM sidebar Updated

--- frontend/src/components/CSMSidebar.js
+++ frontend/src/components/CSMSidebar.js
@@ -1,3 +1,4 @@
+// frontend\src\components\CSMSidebar.js
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { MdDashboard } from "react-icons/md";
